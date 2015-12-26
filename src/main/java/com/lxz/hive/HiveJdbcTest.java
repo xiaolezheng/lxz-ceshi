4604e7ff5358b4db03597ced5187bcbe1e843480
郑晓洛 <zhengxiaoluo@haizhi.com>
2015-12-26 11:07:10 +0800

    add java-object-diff depend

---
+++ src/main/java/com/lxz/hive/HiveJdbcTest.java
@@ -0,0 +1,73 @@
+package com.lxz.hive;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class HiveJdbcTest {
+
+    private static String driverName = "org.apache.hive.jdbc.HiveDriver";
+
+
+    public static void main(String[] args)
+            throws SQLException {
+        try {
+            Class.forName(driverName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        Connection con = DriverManager.getConnection(
+                "jdbc:hive2://localhost:10000/default", "", "");
+        Statement stmt = con.createStatement();
+        String tableName = "pokes";
+        System.out.println("Create table success!");
+        // show tables
+        String sql = "show tables '" + tableName + "'";
+        System.out.println("Running: " + sql);
+        ResultSet res = stmt.executeQuery(sql);
+        if (res.next()) {
+            System.out.println(res.getString(1));
+        }
+
+        // describe table
+        sql = "describe " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(res.getString(1) + "\t" + res.getString(2));
+        }
+
+
+        sql = "select * from " + tableName;
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(String.valueOf(res.getInt(1)) + "\t"
+                    + res.getString(2));
+        }
+
+        sql = "select count(1) from " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(res.getString(1));
+        }
+
+        String insertTemplate = "insert into pokes values(%s,%s)";
+
+        for(int i=1; i<1001;i++){
+            String insertSql = String.format(insertTemplate,i,"test"+i);
+            stmt.execute(insertSql);
+        }
+
+        sql = "select count(1) from " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(res.getString(1));
+        }
+    }
+}
